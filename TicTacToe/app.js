63b38f9d2c763acb3df132c28647a184aa90111a
rogelio lopez <rogejlopez@gmail.com>
2020-11-17 10:11:27 -0500

    finished tictactoe

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -4,73 +4,144 @@ const squares = document.querySelector('.squares');
 const squareTracker = [
     {
         position: 'top-left',
-        picked: 0
+        letter: ''
     },
     {
         position: 'top-middle',
-        picked: 0
+        letter: ''
     },
     {
         position: 'top-right',
-        picked: 0
+        letter: ''
     },
     {
         position: 'middle-left',
-        picked: 0
+        letter: ''
     },
     {
         position: 'center',
-        picked: 0
+        letter: ''
     },
     {
         position: 'middle-right',
-        picked: 0
+        letter: ''
     },
     {
         position: 'bottom-left',
-        picked: 0
+        letter: ''
     },
     {
         position: 'bottom-middle',
-        picked: 0
+        letter: ''
     },
     {
         position: 'bottom-right',
-        picked: 0
+        letter: ''
     }
 ];
 
-// check for matches
-// function (maybe used in event listener)
-// after 4 total picks (2 each)
+// winning combinations
+const winnerCheck = () => {
 
+    let found = '';
+
+    for(let i = 0; i < 4; i++){
+        if (squareTracker[i].letter != 1){
+
+            let sqr1 = squareTracker[i].letter;
+            
+            //top left
+            if(i == 0){
+
+                // added breaks bc of bugs
+                if(sqr1 == squareTracker[3].letter && sqr1 == squareTracker[6].letter){
+                    found = sqr1;
+                    break;
+                }
+                else if(sqr1 === squareTracker[1].letter && sqr1 === squareTracker[2].letter){
+                    found = sqr1;
+                    break;
+                }       
+                else if(sqr1 === squareTracker[4].letter && sqr1 === squareTracker[8].letter){
+                    found = sqr1;
+                    break;
+                }
+
+                /*
+                (sqr1 == squareTracker[3].letter && sqr1 == squareTracker[6].letter)?
+                    found = sqr1 : 
+                    (sqr1 === squareTracker[1].letter && sqr1 === squareTracker[2].letter)?
+                        found = sqr1 :
+                        (sqr1 === squareTracker[4].letter && sqr1 === squareTracker[8].letter)?
+                            found = sqr1 : '';*/
+            }
+            //top middle
+            else if (i == 1){
+                (sqr1 === squareTracker[4].letter && sqr1 === squareTracker[7].letter)?
+                    found = sqr1 : '';
+            }
+            //top right
+            else if(i == 2){
+                (sqr1 === squareTracker[4].letter && sqr1 === squareTracker[6].letter)?
+                    found = sqr1 :
+                    (sqr1 === squareTracker[7].letter && sqr1 === squareTracker[8].letter)?
+                        found = sqr1 : '';
+            }
+            //middle left
+            else if(i == 3){
+                (sqr1 === squareTracker[4].letter && sqr1 === squareTracker[5].letter)?
+                    found = sqr1 : '';
+            }
+        }
+        else{
+            continue;
+        }
+    }
+
+    return found;
+}
+
+// check for matches
 let turn = 0;
+const winner = document.getElementById('winner');
+const winnerMessage = document.getElementsByClassName('winnerMessage')[0];
 
 squares.addEventListener('click', (e) =>{
 
     if (e.target.className == 'square'){
 
+        let p = (e.target.getAttribute('id'));
+
         // X
         if(turn % 2 == 0){
-
-            let p = (e.target.getAttribute('id'));
-
             squareTracker.forEach(square =>{
-                if (square.position == p && square.picked == 0){
-                    
-                }
-                else{
-                    
+                if (square.position == p && square.letter == ''){
+                    e.target.lastElementChild.style.display = 'inline';
+                    square.letter = 'x';
                 }
             });
-
             turn++;
         }
-
         // O
         else{
-
+            squareTracker.forEach(square =>{
+                if (square.position == p && square.letter == 0){
+                    e.target.firstElementChild.style.display = 'inline';
+                    square.letter = 'o';
+                }
+            });
             turn++;
         }
+    }
+
+    // check if won 
+    if (turn > 4 && winnerCheck() != ''){
+        (winnerCheck() == 'x')?
+            winner.innerText = '1' : winner.innerText = '2';
+
+        winnerMessage.style.display = 'inline';
+        setTimeout(() => {
+            location.reload();
+        }, 1500);
     }
 });
